f1f4da653c1c42351f6d860a0181ff4978d5a75f
batuhan <batuhan.caglayan@getir.com>
2021-04-17 20:17:24 +0300

    remove commented out block

--- text-identifier-lambda/dynamo-writer-lambda/src/index.js
+++ text-identifier-lambda/dynamo-writer-lambda/src/index.js
@@ -18,18 +18,4 @@ const processMessage = validate(async (record) => {
 const middyHandler = middy(handler)
 middyHandler.use(sqsPartialBatchFailureMiddleware())
 
-module.exports.handler = middyHandler
-
-// exports.handler = async (event) => {
-//     //console.log('Received event:', JSON.stringify(event, null, 2));
-//     for (const { messageId, body } of event.Records) {
-//         console.log('SQS message %s: %j', messageId, body);
-
-//         const snsBody = JSON.parse(body);
-//         const message = JSON.parse(snsBody.Message);
-        
-//         const result = await commentService.writeComment({ item: message });
-//     }
-
-//     return `Successfully processed ${event.Records.length} messages.`;
-// };
+module.exports.handler = middyHandler;
