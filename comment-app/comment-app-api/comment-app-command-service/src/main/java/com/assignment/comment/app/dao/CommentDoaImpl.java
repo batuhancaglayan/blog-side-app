9f92ac7820b474ce08796ae20ad2d1199a21c4e9
batuhan <batuhan.caglayan@obss.com.tr>
2021-04-19 20:40:05 +0100

    add updatedAt field to dynamo soft delete

--- comment-app/comment-app-api/comment-app-command-service/src/main/java/com/assignment/comment/app/dao/CommentDoaImpl.java
+++ comment-app/comment-app-api/comment-app-command-service/src/main/java/com/assignment/comment/app/dao/CommentDoaImpl.java
@@ -1,6 +1,7 @@
 package com.assignment.comment.app.dao;
 
-import java.util.Collections;
+import java.time.Instant;
+import java.util.HashMap;
 
 import org.springframework.stereotype.Component;
 
@@ -13,22 +14,27 @@ import com.assignment.comment.app.process.TextIdentificationProcess;
 public class CommentDoaImpl implements CommentDoa {
 
 	private DynamoClient dynamoClient;
-	
+
 	private TextIdentificationProcess textIdentificationProcess;
 
 	public CommentDoaImpl(DynamoClient dynamoClient, TextIdentificationProcess textIdentificationProcess) {
 		this.dynamoClient = dynamoClient;
 		this.textIdentificationProcess = textIdentificationProcess;
 	}
-	
+
 	@Override
 	public boolean createComment(CommentModel commentModel) {
 		return this.textIdentificationProcess.startProcess(commentModel);
 	}
-	
+
 	@Override
 	public void softDeleteItem(String commentId) {
-		this.dynamoClient.updateItem("comment-table1", "set isActive = :isActive",
-				Collections.singletonMap(":isActive", 0), new KeyAttribute("id", commentId));
+		this.dynamoClient.updateItem("comment-table", "set isActive = :isActive, updatedAt = :updatedAt",
+				new HashMap<String, Object>() {
+					{
+						put(":isActive", 0);
+						put(":updatedAt", Instant.now().getEpochSecond());
+					}
+				}, new KeyAttribute("id", commentId));
 	}
 }
