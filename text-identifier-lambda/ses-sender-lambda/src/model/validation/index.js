8371b9c0fd98489d5041387c8417b607ed1d8a33
Batuhan Çağlayan <batuhan.caglayan@local.com>
2021-04-19 14:26:18 +0300

    change createdAt & identifiedAt values to long

--- text-identifier-lambda/ses-sender-lambda/src/model/validation/index.js
+++ text-identifier-lambda/ses-sender-lambda/src/model/validation/index.js
@@ -2,8 +2,8 @@ const Joi = require('joi');
 
 const sqsRecordSchema = Joi.object({
     id: Joi.string().required(),
-    createdAt: Joi.date().iso().required(),
-    identifiedAt: Joi.date().iso().required(),
+    createdAt: Joi.number().required(),
+    identifiedAt: Joi.number().required(),
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
     comment: Joi.string().required(),
 })
