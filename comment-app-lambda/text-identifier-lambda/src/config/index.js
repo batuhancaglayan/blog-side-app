a5f2478087f1eaca13c3dd1d6f447a49de7cb61f
batuhan <batuhan.caglayan@getir.com>
2021-04-23 01:38:11 +0300

    fix for typo

--- comment-app-lambda/text-identifier-lambda/src/config/index.js
+++ comment-app-lambda/text-identifier-lambda/src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   region: process.env.REGION || '', // 'eu-central-1',
   verifiedCommentSnsTopic: process.env.VERIFIED_COMMENT_SNS_TOPIC || '', //'arn:aws:sns:eu-central-1:270045217160:BannedCommentSns',
-  bannedCommentSnsTopic: process.env.BANNED_COMMENT_SNS_TOPIC || '', 'arn:aws:sns:eu-central-1:270045217160:VerifiedCommentSns',
+  bannedCommentSnsTopic: process.env.BANNED_COMMENT_SNS_TOPIC || '', //'arn:aws:sns:eu-central-1:270045217160:VerifiedCommentSns',
 }
 
 module.exports = {
