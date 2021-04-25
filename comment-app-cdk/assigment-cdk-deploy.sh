1388bb7cd9c0e2b506eef7038ef3063faeee967d
batuhan <batuhan.caglayan@getir.com>
2021-04-25 20:40:05 +0300

    fixs for deployment script

--- comment-app-cdk/assigment-cdk-deploy.sh
+++ comment-app-cdk/assigment-cdk-deploy.sh
@@ -1,48 +1,60 @@
 #!/usr/bin/env bash
 
 if [[ $# -ge 2 ]]; then
- export CDK_DEPLOY_ACCOUNT=$1
- export CDK_DEPLOY_REGION=$2
- shift; shift
+export CDK_DEPLOY_ACCOUNT=$1
+export CDK_DEPLOY_REGION=$2
+shift; shift
 
- echo '############################# build main app ###################################'
- mvn -f ../comment-app clean install -DskipTests
+echo '############################# build main app ###################################'
+mvn -f ../comment-app clean install -DskipTests
 
- echo '####################### text-identifier-lambda #################################'
- npm install ../comment-app-lambda/text-identifier-lambda
+echo '####################### text-identifier-lambda #################################'
+(
+    cd ../comment-app-lambda/text-identifier-lambda
+    npm install;
+)
 
- echo '####################### dynamo-writer-lambda ###################################'
- npm install ../comment-app-lambda/dynamo-writer-lambda
+echo '####################### dynamo-writer-lambda ###################################'
+(
+    cd ../comment-app-lambda/dynamo-writer-lambda
+    npm install;
+)
 
- echo '####################### ses-writer-lambda ######################################'
- npm install ../comment-app-lambda/ses-sender-lambda
+echo '####################### ses-writer-lambda ######################################'
+(
+    cd ../comment-app-lambda/ses-sender-lambda
+    npm install;
+)
 
- echo '################## comment-table-stream-lambda #################################'
- npm install ../comment-app-lambda/comment-table-stream-lambda
+echo '################## comment-table-stream-lambda #################################'
+(
+    cd ../comment-app-lambda/comment-table-stream-lambda
+    npm install;
+)
 
- echo '###################### comment-app-cloud-infra #################################'
- (
-     cd comment-app-cloud-infra;
-     npm install;
- )
+echo '###################### comment-app-cloud-infra #################################'
+(
+    cd comment-app-cloud-infra;
+    npm install;
+)
 
- echo '############################ cdk bootstrap #####################################'
- (
-     cd comment-app-cloud-infra;
-     npx cdk bootstrap aws://$CDK_DEPLOY_ACCOUNT/$CDK_DEPLOY_REGION "$@"
- )
+echo '############################ cdk bootstrap #####################################'
+(
+    cd comment-app-cloud-infra;
+    npx cdk bootstrap aws://$CDK_DEPLOY_ACCOUNT/$CDK_DEPLOY_REGION "$@"
+)
 
- echo '############################ cdk deploy ########################################'
- (
-     cd comment-app-cloud-infra;
-     npx cdk deploy --all "$@";
- )
+echo '############################ cdk deploy ########################################'
+(
+    cd comment-app-cloud-infra;
+    npx cdk deploy --all "$@";
+)
 
- echo 'Deployment Completed'
+echo 'Deployment Completed'
 
- exit $?
+exit $?
 else
- echo 1>&2 "Provide AWS account and region as first two args."
- echo 1>&2 "Additional args are passed through to cdk deploy."
- exit 1
+echo 1>&2 "Provide AWS account and region as first two args."
+echo 1>&2 "Additional args are passed through to cdk deploy."
+exit 1
 fi
